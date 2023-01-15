1fc8906d45427781af55dd96224469a5a6865929
davi_ <davi.frta@gmail.com>
2023-01-15 15:08:34 -0300

    fix url

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
     });
 
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, 'dist', 'agent', 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
